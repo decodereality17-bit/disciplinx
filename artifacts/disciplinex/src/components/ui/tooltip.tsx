c6ff190f66225ed2435aec518521d031a5b86866
decodereality17-bit <decodereality17@gmail.com>
2026-05-29 08:42:17 +0000

    Add placeholder UI components

---
+++ artifacts/disciplinex/src/components/ui/tooltip.tsx
@@ -0,0 +1,4 @@
+export const Tooltip = ({ children }: any) => children;
+export const TooltipTrigger = ({ children }: any) => children;
+export const TooltipContent = ({ children }: any) => children;
+export const TooltipProvider = ({ children }: any) => children;
